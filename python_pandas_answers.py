c3add7458e00344fc384baff8d828d0308321660
Inna Tunik <inntunik@gmail.com>
2016-09-28 18:39:42 -0400

    Tabs to Spaces

--- python_pandas_answers.py
+++ python_pandas_answers.py
@@ -3,24 +3,24 @@ import pandas as pd
 def main():
     # Load the CSV file into a data frame.
     # http://pandas.pydata.org/pandas-docs/stable/generated/pandas.read_csv.html
-	df = pd.read_csv("sample_dataset.csv")
+    df = pd.read_csv("sample_dataset.csv")
 
     # sum of spend column
     # http: // pandas.pydata.org / pandas - docs / stable / generated / pandas.DataFrame.sum.html
-	print df["spend"].sum()
+    print df["spend"].sum()
 
     # standard deviation of pixel 1 column
     # http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.std.html
-	print df["pixel_1"].std()
+    print df["pixel_1"].std()
 
     # count of unique values in the utm column
     # http://pandas.pydata.org/pandas-docs/version/0.17.0/generated/pandas.Series.unique.html
-	print df["utm"].nunique()
+    print df["utm"].nunique()
 
-	return None
+    return None
 
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
