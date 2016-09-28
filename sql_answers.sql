0241e58ac3b04d99424de72d4b9e984117acde6e
Inna Tunik <inntunik@gmail.com>
2016-09-28 18:51:15 -0400

    SQL answers

--- sql_answers.sql
+++ sql_answers.sql
@@ -1,23 +1,13 @@
 -- Question 1
-CREATE VIEW
-    q1table
-AS
+CREATE VIEW q1table AS
     SELECT
         adwords.spend,
         adwords_conversions.conversions,
-        adwords.spend/adwords_conversions.conversion
-    AS
-        "cost per conversion"
-    FROM
-        adwords
-    INNER JOIN
-        adwords_conversions
-    ON
-        adwords.account = adwords_conversions.account
-    WHERE
-            conversion_type_name = Ticket Purchase
-        AND
-            adwords.account = "Account X"
+        adwords.spend/adwords_conversions.conversion AS "cost per conversion"
+    FROM adwords
+    INNER JOIN adwords_conversions ON adwords.account = adwords_conversions.account
+    WHERE conversion_type_name = "Ticket Purchase"
+      AND adwords.account = "Account X"
     ORDER BY adwords.date;
 
 
@@ -27,10 +17,6 @@ SELECT
     bing.impressions as "bing_impressions",
     bing.conversions,
     bing.spend/bing.conversions as "bing_cost_per_converion"
-FROM
-    bing
-JOIN
-    q1table
-ON
-    q1table.account = bing.account
+FROM bing
+INNER JOIN q1table ON q1table.account = bing.account
 GROUP BY bing.date;
